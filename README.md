dbd5164bf6be118cba041e5fbc2f494fd02d1b61
DeanLanier <dean.lanier@gmail.com>
2019-07-15 10:26:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# openlayers-video-nqh2yb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/openlayers-video-nqh2yb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { OlMapComponent } from './olMap/olMap.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule} from '@angular/material';
+
+@NgModule({
+  imports:      [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  declarations: [ AppComponent, HelloComponent, OlMapComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
